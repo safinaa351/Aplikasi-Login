917623215d73764b021285a0b9a6fe89a66512da
safina ayu <121283294+safinaa351@users.noreply.github.com>
2024-09-12 19:49:25 +0700

    update fungsionalitas di login script

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -14,10 +14,16 @@ const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
+    const expectedEmail = 'admin@dicoding.com';
+const expectedPassword = 'superpassword';
+
+loginFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+    if (email == expectedEmail && password == expectedPassword) {
+      goToHome();
+    } else {
+      showPopUp();
+    }
 });
